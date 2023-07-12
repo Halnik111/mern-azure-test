33fec86f14127549900a64c065cfcc5ec960f175
Nikolas <halonikolas@gmail.com>
2023-07-12 12:18:35 +0200

    -

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ app.post("/create", (req, res) => {
 });
 
 app.use(express.static("./client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+app.get("/", (req, res) => {
+  res.send('Working');
 });
 
 app.listen(port, () => {
