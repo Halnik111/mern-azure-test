04b8032202717225f4a3bab417a23c1ecf2074fc
Nikolas <halonikolas@gmail.com>
2023-07-12 13:09:35 +0200

    -

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 const port = process.env.PORT || 3001;
 
 const app = express();
