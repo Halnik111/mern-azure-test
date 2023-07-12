329351ba7fb9faf36c06b09a45e80a73f47ab717
Nikolas <halonikolas@gmail.com>
2023-07-12 13:03:38 +0200

    -

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3001;
-const path = require('path');
 
 const app = express();
 
